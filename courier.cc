9dcb6e474b0c0dfda24a7e0770de42d45e11ce02
ValesJakub <kubavales39@gmail.com>
2023-12-08 17:39:15 +0100

    Rozvazka kuryra

--- courier.cc
+++ courier.cc
@@ -1,16 +1,56 @@
 #include "simlib.h"
 
+int balicky = 0;            // počítač balíčků
+
 Store kapacita_auta("Prostor", 80);
 Store sklad("Polozky", 9999);       // asi pak napsat nejaky mega cislo proste
 Facility Kuryr("kuryr");
+Facility Rozvoz("Je co rozvazet");
 
-int balicky = 0;            // počítač balíčků
+
+class Rozvazeni : public Process {
+public:
+    void Behavior() {
+    Store balicky_v_aute("Balicky", balicky);
+        //printf("%d", balicky_v_aute.Capacity());
+
+        Seize(Kuryr);
+        double tvstup = Time;
+        int nevyzvednute = 0;
+        Wait(300);          // Priprava na rozvazeni
+        while(!balicky_v_aute.Full())       // Dokud je co -> rozvazej
+        {
+            Seize(Rozvoz);
+
+            Enter(balicky_v_aute, 1);
+            Wait(Exponential(300));         // Dojezd na misto
+
+            if(Random()<= 0.90)
+            {
+                // Uspesne doruceni
+                Wait(Exponential(60));
+                Release(Rozvoz);
+            }
+            else
+            {
+                // Neni doma
+                nevyzvednute++;
+                Release(Rozvoz);
+            }
+        }
+        printf(" ktere rozvezl za: %.1fh rozvezl a vrací se s: %d\n\n", (Time - tvstup)/60/60, nevyzvednute);
+        Leave(sklad, nevyzvednute);
+        Release(Kuryr);
+    }
+};
+
 
 class Nakladka : public Process {
 public:
     void Behavior() {
-        double tvstup = Time;
         Seize(Kuryr);
+        double tvstup_nak = Time;
+        balicky = 0; 
         while(!kapacita_auta.Full())
         {
             if(Random()<= 0.80)
@@ -43,24 +83,16 @@ public:
         // Konec nakládání
         Release(Kuryr);
         Leave(kapacita_auta, 80);
-        printf("balicky: %d\n", balicky);
-        balicky = 0; 
+        printf("Za: %.1fmin nalozil: %d balicku", (Time - tvstup_nak)/60, balicky);
+        (new Rozvazeni)->Activate();
+        (new Nakladka)->Activate();
     }
 };
-
-// Tady nevím, jak to jinak generovat, takže zatím takhle
-class Generator : public Event {
-    void Behavior() {
-	(new Nakladka)->Activate();
-		Activate(Time+10);      // + 10 je tady jen tak, nevím co tam dat
-    }
-};
-
 
 int main()
 {
-    Init(0, 100000);
-    (new Generator)->Activate();
+    Init(0, 1000000);            //random cislo
+    (new Nakladka)->Activate();
     Run();
-    
+    printf("\n");
 }
